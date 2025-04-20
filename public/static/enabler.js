4c3c92e7fbcd5e38c3000f0e545330a14dd827c3
Alex Toucan <alextoucan.at@gmail.com>
2025-04-20 15:10:21 -0500

    Second commit.

--- public/static/enabler.js
+++ public/static/enabler.js
@@ -1,3 +1,3 @@
-      const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 
-      const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
