4e4a0d69fe97e0bc0b98a776bb0edf23f603a254
Alex Toucan <alextoucan.at@gmail.com>
2025-04-21 16:02:28 -0500

    Add section.

---
+++ public/static/figure.js
@@ -0,0 +1,17 @@
+const section = document.querySelector("#general-info");
+const figure = document.querySelector("#general-info figure");
+const paragraph = document.querySelector("#general-info p");
+
+if (section && figure && paragraph) {
+    // Calculate the new height
+    const figureHeight = figure.offsetHeight;
+    const paragraphHeight = paragraph.offsetHeight;
+    const sectionHeight = section.offsetHeight;
+
+    const newMinHeight = sectionHeight + (figureHeight - paragraphHeight);
+
+    // Apply the calculated height to #section
+    section.style.minHeight = `${newMinHeight}px`;
+} else {
+    console.error("Unable to find one or more required elements.");
+}
